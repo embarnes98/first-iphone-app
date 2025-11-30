5447167cf5dd64d7efd12ea41ed14ef7da1f648f
Edward Barnes <embarnes98@gmail.com>
2025-11-30 18:30:09 +0000

    Initial Commit

---
+++ embarnes-hello-world/embarnes_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  embarnes_hello_worldApp.swift
+//  embarnes-hello-world
+//
+//  Created by Edward Barnes on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct embarnes_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
